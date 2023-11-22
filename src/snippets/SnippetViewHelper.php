fd82b38acd17153669613709a1f3123d7c66c274
Timothy Williamson <atimiw@gmail.com>
2023-11-22 13:43:29 +0000

    Tidying up redundant code in SnippetViewHelper.php.

--- src/snippets/SnippetViewHelper.php
+++ src/snippets/SnippetViewHelper.php
@@ -8,12 +8,10 @@ class SnippetViewHelper
     public static function displaySingleSnippetMini(Snippet $snippet, array $languages): string
     {
         $output = '<div class="snippet-container">';
+        
         $output .= "<div class='snippet-filename'>$snippet->filename</div>";
         $output .= "<textarea class='snippet-codeblock-mini' readonly>$snippet->codesnippet</textarea>";
-
-        // var_dump($snippet->language_id);
         $output .= LanguageViewHelper::displaySingleLanguage($languages[$snippet->language_id - 1]);
-        // $output .= LanguageViewHelper::displaySingleLanguage($languages[2]);
         
         $output .= '</div>';
 
