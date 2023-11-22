582ab122f96923f5da8e0cf28b9e88a5b67b0588
Timothy Williamson <atimiw@gmail.com>
2023-11-22 14:26:28 +0000

    Updated collection.sql with latest db setup and data.:

--- collection.sql
+++ collection.sql
@@ -7,7 +7,7 @@
 #
 # Host: 127.0.0.1 (MySQL 11.1.2-MariaDB-1:11.1.2+maria~ubu2204)
 # Database: collection
-# Generation Time: 2023-11-22 08:52:41 +0000
+# Generation Time: 2023-11-22 14:25:50 +0000
 # ************************************************************
 
 
@@ -37,13 +37,13 @@ LOCK TABLES `entries` WRITE;
 
 INSERT INTO `entries` (`id`, `title`, `description`)
 VALUES
-	(1,'Nested For','Example of a nested for loop.\n\n(Test for no associated snippet attached).'),
 	(2,'Codewars 6 kyu: Multiples of 3 or 5','If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.\n\nFinish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.\n\nAdditionally, if the number is negative, return 0.\n\nNote: If the number is a multiple of both 3 and 5, only count it once.'),
 	(3,'Fading button','Adding a fading effect to an HTML5 button using CSS'),
 	(4,'Link Tables','Solves the problem of needing to have more than one ID, allows many to many relationships. \n\nCreate a new table that only holds ‘parent’ and matching ‘child’ IDs.'),
 	(5,'PHPUnit','Inside your project directory:\n	Have a subdirectory named tests\n	Inside the tests directory, you must have a file for every PHP file inside your application (with the same filenames)\n	Test files should include the real files using require to get access to the actual function to be tested'),
 	(6,'Props in React','Data can be passed into components via props. This data can then be accessed by the specified props and the dot accessor.'),
-	(7,'Media query','CSS feature to apply different styling depending on different “media”.');
+	(7,'Media query','CSS feature to apply different styling depending on different “media”.'),
+	(21,'PHPUnit example (cow)','An example of setting up unit tests using PHPUnit');
 
 /*!40000 ALTER TABLE `entries` ENABLE KEYS */;
 UNLOCK TABLES;
@@ -102,7 +102,10 @@ VALUES
 	(6,'Button.jsx','function Button (props) { \n	return ( \n	<a className={\"button \" + props.colour} href={props.link} target=\"_blank\">{props.text}</a> \n	)\n} ',6,1),
 	(7,'Button.css','.button { \n	background-color: teal; \n	font-size: 1.5rem; \n	padding: 10px; \n	border-radius: 8px; \n	box-shadow: 2px 2px 4px black; \n \n	margin: 20px; \n} \n \n.button.red { \n	background-color: red; \n} \n \n.button.green { \n	background-color: green; \n} ',6,4),
 	(8,'App.jsx','function App() { \n\n	return ( \n	<> \n	<Button text=\"Red\" link=\"http://www.youtube.com\" colour=\"red\"/> \n  \n	<Button text=\"Learn more\" link=\"http://www.youtube.com\" /> \n \n	<Button text=\"Green\" link=\"http://www.facebook.com\" colour=\"green\"/> \n	</> \n	) \n} ',6,1),
-	(9,'','/* mobile first - default styling will be a column layout */\nmain {\n  	display: flex;\n	flex-direction: column;\n	flex-wrap: nowrap;\n}\n\n/* we create a breakpoint at 400px screen width - any bigger and the layout will be a row */\n@media screen and (min-width: 400px) {\n	flex-direction: row;\n  	flex-wrap: wrap;\n}\n\n/* we can choose to have even more breakpoints if we care that much */\n@media screen and (min-width: 800px) {\n  	flex-wrap: nowrap; /* on a large screen, the row is not allowed to wrap */\n}',7,4);
+	(9,'','/* mobile first - default styling will be a column layout */\nmain {\n  	display: flex;\n	flex-direction: column;\n	flex-wrap: nowrap;\n}\n\n/* we create a breakpoint at 400px screen width - any bigger and the layout will be a row */\n@media screen and (min-width: 400px) {\n	flex-direction: row;\n  	flex-wrap: wrap;\n}\n\n/* we can choose to have even more breakpoints if we care that much */\n@media screen and (min-width: 800px) {\n  	flex-wrap: nowrap; /* on a large screen, the row is not allowed to wrap */\n}',7,4),
+	(20,'Cow.php','<?php\r\n\r\ndeclare(strict_types=1);\r\n\r\nclass Cow\r\n{\r\n    public function eat(Grass $food): string\r\n    {\r\n        return \"mmmm {$food->getFoodType()}\";\r\n    }\r\n}',21,2),
+	(21,'Grass.php','<?php\r\n\r\ndeclare(strict_types=1);\r\n\r\nclass Grass\r\n{\r\n    private string $foodType = \'Grass\';\r\n\r\n    public function getFoodType(): string\r\n    {\r\n        return $this->foodType;\r\n    }\r\n}',21,2),
+	(22,'CowTest.php','<?php\r\n\r\nrequire_once \'src/Cow.php\';\r\nrequire_once \'src/Grass.php\';\r\n\r\nuse PHPUnit\\Framework\\TestCase;\r\n\r\nclass CowTest extends TestCase\r\n{\r\n    public function test_eat_success(): void\r\n    {\r\n        // Because the Cow now depends on another object (Grass)\r\n        // we don\'t just pass the dependency into the Cow\r\n        // because that is not a unit test - testing 2 things at once\r\n\r\n        // Instead, we make a fake \'mock\' version of the dependency\r\n        $grassMock = $this->createMock(Grass::class);\r\n        // We look at what methods the mock needs to have\r\n        // and we make a dumb fake version of those methods\r\n        $grassMock->method(\'getFoodType\')->willReturn(\'Grass\');\r\n\r\n        // Set the result we expect\r\n        $expected = \'mmmm Grass\';\r\n\r\n        $cow = new Cow();\r\n        // We pass our mock version of grass into a cow\r\n        $result = $cow->eat($grassMock);\r\n\r\n        // Perform an assertion\r\n        $this->assertEquals($expected, $result);\r\n    }\r\n\r\n    // With malformed tests, we are making sure the function /\r\n    // object properly rejects the wrong kind of data\r\n    public function test_eat_malformed(): void\r\n    {\r\n        // Creating a test input that is the wrong data type\r\n        $testFood = [\'Grass\', \'Hay\'];\r\n\r\n        // Telling PHPUnit that we WANT to get a type error\r\n        $this->expectException(TypeError::class);\r\n\r\n        $cow = new Cow();\r\n        $cow->eat($testFood);\r\n        // Trigger the type error, because we\'re passing\r\n        // in the wrong datatype\r\n    }\r\n}',21,2);
 
 /*!40000 ALTER TABLE `snippets` ENABLE KEYS */;
 UNLOCK TABLES;
