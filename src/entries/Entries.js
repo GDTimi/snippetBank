41cbadce0adccb35054fcb90b0d404c11eeadf70
Timothy Williamson <atimiw@gmail.com>
2023-11-22 09:38:08 +0000

    Fixed the innerhtml in Entries.js to not inject a form into a form.

--- src/entries/Entries.js
+++ src/entries/Entries.js
@@ -7,19 +7,17 @@ function addSnippet() {
     div.setAttribute('class', 'snippet-container');
     div.setAttribute('id', snippetID);
     div.innerHTML = `
-            <form class="form" method="Post">
                 <label for="filename">Filename:</label>
-                <input type="text" name="filename" value="" required="true"/>  
+                <input type="text" name="filename" value=""/>  
                 
                 <label for="snippet">Snippet*:</label>
                 <textarea class='snippet-codeblock-midi' name="snippet" required="true"></textarea>    
                 
-                <label for="category_id">Category ID:</label>
+                <label for="category_id">Category ID*:</label>
                 <select id="category_id" name="category_id" required="true">
                         ${languageOptionsHTMLString}              
                     ?> 
-                </select>
-            </form>       
+                </select>      
     `;
     document.getElementById('snippet-hold').appendChild(div);
 
